39be1b10a551ea3fce5ee4d804132bd6ef4350de
jainaman0327 <96785277+jainaman0327@users.noreply.github.com>
2022-07-13 19:53:34 +0530

    Add files via upload

---
+++ Digital Marketing Assignment 1.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Question 1. What is the definition of digital marketing ?
+# 
+# Ans: Digital marketing, also called onlin marketing,
+# is the promotion of brands to connect with potential customers
+# using the internet and other forms of digital communication.
+# This includes not only email, social media, and web-based advertising,
+# but also text and multimedia messages as a marketing channel.
+# 
+# Essentially, if a marketing campaign involves digital communication,
+# it's digital marketing.
+
+# Question 2. Why is digital marketing so important.?
+# 
+# Ans: Digital marketing is important because it connects a business with its customers
+# when they are online, and is effective in all industries.it connects bussinesses with 
+# ideal customers when they are on google through SEO and PPC, on social media with 
+# social media marketing, and through email with email marketing.
+
+# Question 3. What are the essential functions of digital marketing?
+# 
+# Ans: The objective of digital marketing is to develop strong and
+# innovative strategies to promote the business brand, products, and services.
+# A digital marketing professional is expected to effectively
+# use all marketing tools and techniques like PPC, SEO, SEM, email, social media, 
+# and display advertising. A typical digital marketing job description will cover
+# all the above aspects, and more:
+#         
+# * Identify trends and insights
+# 
+# * Allocate marketing investments 
+# 
+# * Plan and direct marketing campaigns
+# 
+# * Manage an organization's website and maintain it,
+# keeping best practices in mind
+# 
+# * Optimize content for the website and social media platforms
+# 
+# * Work with various content formats such as blogs, videos, 
+# audio podcasts, etc.
+# 
+# * Track the website traffic flow
+
+# Question 4. What is the defination of traditional marketing.?
+# 
+# Ans: Traditional marketing refers to any type of marketing
+# that isn't online. This means print, broadcast, direct mail,
+# phone, and outdoor advertising like billboards. From newspapers 
+# to radio, this method of marketing helps reach targeted audiences.
+
+# Question 5.In digital marketing, how do you locate your target audience?
+# 
+# Ans: The following steps should help locate target auduence.
+#     
+# 1.Analyze Your Customer Base and Carry Out Client Interviews.
+# 
+# 2. Conduct Market Research and Identify Industry Trends.
+# 
+# 3. Analyze Competitors.
+# 
+# 4. Create Personas.
+# 
+# 5.Define Who Your Target Audience Isn’t.
+# 
+# 6.Continuously Revise.
+
+# Question 6. In traditional marketing, how do you find your target audience?
+# 
+# Ans: Traditional marketing is offline.  These channels include magazine, newspaper,
+# television, radio, direct mail and billboards to name a few. Traditional marketing 
+# is all about reaching a large general audience. The more people that see your ad,
+# the more leads you have the potential to attract. 
+# 
+# If your target audience is an older demographic, traditional advertising can be 
+# very effective. Audiences ages 50+ spend almost twice as much time reading the
+# newspaper and watching TV as compared to those ages 21-34.
+# 
+# Traditional marketing is good for businesses looking to build a large local audience.
+# A small business can easily compete for the attention of a local audience through flyers,
+# billboards, and radio commercials rather than trying to compete for digital space against 
+# larger businesses.
+# 
+# TV or radio commercials will play multiple times and remind the audiences of the business,
+# whereas digital marketing items may be skipped.
+# 
+# 
