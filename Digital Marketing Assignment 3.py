6d97abec8a12383862bf69154d359d257901dfd0
jainaman0327 <96785277+jainaman0327@users.noreply.github.com>
2022-07-20 02:36:13 +0530

    Add files via upload

---
+++ Digital Marketing Assignment 3.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Question 1. What are the different types of marketing platforms?
+# 
+# Ans: Types of marketing platforms
+#     
+# *Email marketing platforms.
+# 
+# *Social media marketing platforms.
+# 
+# *Content marketing platforms.
+# 
+# *Search engine optimization marketing.
+# 
+# *Video marketing platforms.
+# 
+# *Brand collaboration and influencer marketing.
+# 
+# *Online marketplaces.
+# 
+# *Service-oriented platforms.
+# 
+# *Pay-per-click (PPC)
+# 
+# *All-in-one marketing platforms
+
+# Question 2.Make a list of the different types of digital marketing.
+# 
+# Ans: Types of Digital Marketing
+#     
+# * Search Engine Optimization (SEO) 
+# 
+# * Pay-per-Click (PPC)
+# 
+# * Social Media Marketing.
+# 
+# * Content Marketing. 
+# 
+# * Email Marketing. 
+# 
+# * Mobile Marketing.
+# 
+# * Marketing Analytics. 
+# 
+# * Affiliate Marketing.
+
+# Question 3.What exactly are the many sorts of digital marketing?
+# 
+# Ans: Type of Digital marketing
+#     
+# 1.Search Engine Optimisation (SEO)
+# 
+# 2.Pay Per Click Advertising (PPC)
+# 
+# 3.Content Marketing
+# 
+# 4.Search Engine Marketing (SEM)
+# 
+# 5.Social Media Marketing
+# 
+# 6.Influencer Marketing
+# 
+# 7.Email Marketing
+# 
+# 8.Mobile Marketing
+# 
+# 9.Affiliate Marketing
+# 
+# 10.Augmented Reality and Virtual Reality Marketing
+
+# Question 4.In terms of digital marketing, what is a website?
+# 
+# Ans: For instance, your website may be a digital marketing
+# e-commerce site that sells software and services.
+# The site's product and service pages categorized. 
+# Your click map will track user activity for images, text, 
+# buttons, and page elements visitors click for each page.
+# Various tools are used to generate a click map.
+
+# Question 5. What is a domain name system, and how does it work?
+# 
+# Ans: The Domain Name System (DNS) turns domain names into IP addresses,
+# which browsers use to load internet pages. Every device connected to 
+# the internet has its own IP address, which is used by other devices to locate the device.
+
+# Question 6. What is a landing page for a business?
+# 
+# Ans: A standalone web page that potential customers
+# can “land” on when they click through from an email, ad, 
+# or other digital location. A landing page aims to capture
+# information from contacts in exchange for something of value,
+# such as a retail offer code or business-to-business (B2B) insights
+# in the form of a white paper.
