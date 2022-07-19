75cc5cef697b5e52e2e4edd8f4a79d4e1d2533e8
jainaman0327 <96785277+jainaman0327@users.noreply.github.com>
2022-07-20 01:48:55 +0530

    Add files via upload

---
+++ Digital marketing Assignment 4.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Question 1.What's the difference between a landing page and a website?
+# 
+# Ans: Unlike a landing page, a website can have multiple pages of information with different goals and purposes.
+# 
+# Not each page on a website is as highly focused on conversions as a landing page.
+# 
+# For instance, a business website may want users to get information about all the products and
+# services they are offering.
+# It may also give users multiple calls to action to choose from (e.g. make a purchase, 
+# ask for quote, join email list, follow on social media, and more).
+# 
+# Different types of websites would have different goals and a website gives them more ways to engage the audience.
+# 
+# A website could also be supported by multiple landing pages for their marketing campaigns.
+# 
+# For instance, a membership website may utilize a landing page for a subscription plan or an eCommerce store may
+# want to create a pre-launch landing page for an upcoming product.
+
+# Question 2. Using the GoDaddy website builder, you may create a website.
+# 
+# Ans: Using the GoDaddy website builder we creat a website in some steps:
+#         
+# 1.Choose Your Site Type
+# 2.Select Your Industry and Website Title
+# 3.Start Editing Your Site: Pick Your Theme
+# 4.Customize Your Design: Add Your Own Images
+# 5.Add Your Own Text
+# 6.Add New Sections
+# 7.Add Pages
+# 8.Manage Your Site Settings
+# 9.Connect a Custom Domain
+# 10.Preview and Publish Your Site
+# 11.Promote Your Website
+
+# Question 3.What is the definition of content marketing?
+# 
+# Ans: Content marketing is a strategic marketing approach
+# focused on creating and distributing valuable, relevant,
+# and consistent content to attract and retain a clearly
+# defined audience — and, ultimately, to drive profitable 
+# customer action.
+
+# Question 4. What is Content Marketing's Impact?
+# 
+# Ans:Content marketing is important because
+# it answers your audience's questions and helps
+# you build trust, develop relationships, improve
+# conversions, and generate leads. In today's age,
+# customers expect high-quality, consistent content
+# from their favorite brands.
+
+# Question 5. Would you mind writing down the origins of content marketing?
+# 
+# Ans: The term “content marketing” itself was coined by John F. Oppedahl at
+# a journalism conference in 1996. This put a name to a practice that had
+# been taking place for hundreds of years, and gave way to what we consider 
+# “modern” content marketing.
+
+# Question 6.What are some of the advantages of content marketing?
+# 
+# Ans: Advantages of content marketing
+# 
+# Content marketing helps you to: Build brand awareness, trust and
+# loyalty - people consuming your content will start to build an
+# impression of your brand. Publishing informative, well-researched
+# content means your business will be seen as authoritative and trustworthy.
