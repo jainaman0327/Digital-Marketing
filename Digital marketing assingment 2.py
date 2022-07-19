1d9cad6943b342864a437f07c4545d4b4e105257
jainaman0327 <96785277+jainaman0327@users.noreply.github.com>
2022-07-19 23:58:11 +0530

    Add files via upload

---
+++ Digital marketing assingment 2.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Question 1. In traditional marketing, what is cost analysis?
+# 
+# Ans:If you take a look at the marketing trends over the past 50 years,
+# you will be able to see a similarity. They all revolve around Audio,
+# Written word, and visual. From the traditional side, it is Radio, Newspaper,
+# and TV ads. From a Digital side, it is Podcasts, Blogs, and social media verticals.
+#   Back when the radio first became a popular marketing vertical advertising was very cheap, as was television, and newspaper ads. After years of people seeing the 
+# benefit of these ads, the market became saturation the ads became more and more expensive.
+# I can see this same trend with Social Media Ads, and it is terrifying. Since 2018,
+# the price of Facebook ads has increased by 4x to reach 1,000 people! This will continue to
+# happen until the market is so saturated the average joe business owner will NOT be able to
+# afford a Facebook ad. 
+# The good this is that digital marketing is WAY cheaper and more effective than Traditional Methods.
+
+# Queestion 2.What is Digital Marketing Cost Analysis?
+# 
+# Ans: Digital marketing has helped to bring down marketing costs
+# for some businesses, but it’s still important to understand if 
+# the marketing money being spent is bringing in new leads towards 
+# generating revenue for your business. One way to do that is to 
+# perform a cost-effectiveness analysis – not only does this help
+# to measure your reach, but it helps you to understand if your money
+# are being spent wisely and effectively.
+# 
+# Measuring against goals
+# 
+# The first step to understanding the cost effectiveness ratio of your digital marketing efforts is to understand what the goals of your marketing are.
+# 
+# Understanding not all cost is monetary
+# 
+# While most people performing a cost-effectiveness analysis of their digital marketing efforts might simply try to understand campaign bottom-line dollars spent per impression, other businesses understand that the time it takes to brainstorm campaign concepts, create content, design digital advertising all have an inherent “cost” in the form of time or salary. The cost of marketing is beyond the money paid to a publisher to promote a piece or host an ad – don’t forget to factor in the “hidden” costs of creating digital marketing content.
+# 
+
+# Question 3. What are the four p's of marketing?
+# 
+# Ans: The four Ps are product, price, place, and promotion.
+# They are an example of a “marketing mix,” or the combined 
+# tools and methodologies used by marketers to achieve their
+# marketing objectives.
+
+# Question 4. Can you give some examples of digital advertisements?
+# 
+# Ans: Social media ads appear on social media platforms like Facebook,
+# Instagram, Snapchat, Reddit, LinkedIn, etc. These ads can be display 
+# ads, native ads, video ads, audio ads, remarketing ads or mobile ads.
+
+# Question 5.What are the advantages and disadvantages of digital marketing?
+# 
+# Ans:Advantages And Disadvantages Of Digital Marketing:
+# Digital marketing is
+# a method of advertising and marketing through online channels using digital devices. 
+# This marketing method basically makes use of various online features like website design,
+# search engine optimization (SEO), and social media marketing. It requires knowledge and
+# proficiency in handling the digital platforms to make the best use of it in marketing campaigns. 
+# Extensive use of digital devices provides a better opportunity to reach a wider base of customers.
+# 
+# Digital advertising and marketing becomes an effective promotional strategy for companies to reach 
+# a worldwide market. In this era of modern technology, it becomes necessary for organizations to take 
+# advantage of digital techniques to explore and access the mass market to remain at the competitive edge.
+# 
+# Advantages of Digital Marketing
+# 
+# *Wider Reach: Digital marketing helps improve awareness about products and services through an online
+# platform to reach a wide base of customers across the world. This strategy allows even the start-up 
+# companies to have equal access to the customers while competing with big enterprises in the traditional market.
+# 
+# *Brand Development: It is necessary to establish a brand image for the product or services through the marketing 
+# campaign so that customers consider it a reliable and trusted name. Digital marketing helps to build the brand 
+# in various ways of direct campaigning and personalized approach. This technique makes customers more engaged and
+# allows them to experience the unique selling proposition and value of the brand. This leads to a sense of positivity
+# among the customers towards the brand.
+# 
+# *Personalization: The strategy of digital marketing is more effective because of its ability to interact with prospects 
+# and get feedback. Bringing the audience closer and approaching them with the product that meets their requirement,
+# helps to create brand loyalty and reputation. Digital marketing provides a platform for personalized marketing by
+# designing specialized content for customers. The purpose is to make customers feel that the brand is addressing
+# their individual needs.
+# 
+# *Accessibility: Marketing methods using digital platforms provide the opportunity for both sellers and buyers to get
+# easy access with the flexibility of time and location. The potential customers get an experience of a virtual shop with
+# information about services and availability of product range. The customers can leave reviews and recommendations 
+# that guide the organizations to plan their marketing strategies accordingly to respond to customer requirements more effectively.
+# 
+# *Better Engagement: Digital marketing allows the brand to create better awareness among the customers and get recognition from more people. 
+# A suitable promotional strategy using blogs, social media content, and promotional offers allow the brand to get a hold of the customer’s 
+# attention and build credibility.
+# 
+# *Lower Cost: The popularity of digital marketing is attributed to its cost-effective way of handling business activities. 
+# The cost involved in digital marketing is much less in comparison to traditional marketing campaigns. 
+# The digital marketing methodology is easier to implement with a limited budget and infrastructure which
+# is beneficial especially for small businesses.
+# 
+# *Trackable results: The digital analytical tools and other online metrics help to track how effective the marketing 
+# campaign has been in terms of customer reach and response. It provides necessary information about the extent of customer 
+# engagement and response to your website and advertising.
+# 
+# *Improved conversion rates: The digital marketing process creates a seamless and immediate connection with potential customers
+# with the convenience of making a purchase just with a few clicks on your website. This leads to better chances of making a purchase
+# in response to marketing campaigns.
+# 
+# Disadvantages of Digital Marketing
+# 
+# *Skills Requirement: There is a need of gathering the right knowledge and expertise on the digital platform to carry out the marketing 
+# activities successfully. With regular advancements in technology, it is essential to remain updated on tools, platforms, and methods to
+# quickly respond to any changes.
+# 
+# *Time-consuming: We need to plan and implement various tasks related to optimization of online advertising and creating suitable content
+# for promotions that can take up a lot of time. It’s also important to regularly track the results and modify strategies accordingly to ensure
+# a return on investment.
+# 
+# *High competition: A wider reach to the global audience with digital marketing also requires building capabilities to face global competition.
+# It can be a challenge to establish the brand against more competitors and to grab attention among the many digital channels campaigning in real-time.
+# 
+# *Complaints and feedback: The digital platforms allow for feedback or criticism by anyone about your product or services that can be visible to your
+# audience through social media networks and review channels. Any negative review can create an adverse impact on some part of your customers unless 
+# you have a strong foothold through brand loyalty and efficient customer service. There is a challenge to maintain the brand reputation by responding 
+# to negative feedback effectively.
+# 
+# *Dependability on Technology: Digital marketing is purely based on digital technology and internet access. There can be instances of failure when 
+# the links may not work, the website may not respond, and user access gets hampered. This may create dissatisfaction among the prospective customers 
+# that forces them to switch to other brands. A regular checkup or maintenance of the website is necessary to cope up with the constantly changing 
+# technological environment.
+# 
+# Security and Privacy Issues: The more use of online platforms demands better security and website protection. For a digital marketer, securing and 
+# safeguarding the network connections by using firewalls and good antivirus is always desirable. The legal formalities for protecting and using customer
+# data in digital marketing strategies need to be completed to avoid any complaints on data breaches.
+
+# Question 6.Make a list of the many types of digital marketing?
+# 
+# Ans:Types of Digital Marketing
+#     
+# * Search Engine Optimization (SEO) 
+# * Pay-per-Click (PPC) 
+# * Social Media Marketing.
+# * Content Marketing. 
+# * Email Marketing. 
+# * Mobile Marketing. 
+# * Marketing Analytics. 
+# * Affiliate Marketing.
