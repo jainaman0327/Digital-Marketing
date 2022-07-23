3ebf665c0b1ed1b6a483aa8f0198bce480a0141d
jainaman0327 <96785277+jainaman0327@users.noreply.github.com>
2022-07-24 01:10:35 +0530

    Add files via upload

---
+++ Digital Marketing Assingment 5.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Question 1.What's the difference between traditional marketing and content marketing?
+# 
+# Ans:Traditional marketing: Traditional marketing is any type of marketing that is forced into
+# the view of a person. For example, TV commercials, radio ads, print ads, brochures and the like.
+# These types of ads can be effective at reaching a large audience, but a large budget is required to do so.
+# Larger businesses will often use traditional marketing successfully because they can advertise in venues
+# where their ads will get a lot of exposure, such as on a national cable channel during prime time hours or
+# on the back of a magazine with a huge subscription base.
+
+# Question 2.What is content, and how do you write different content?
+# 
+# Ans:Content writing is the process of writing, editing, and publishing
+# content in a digital format.
+# 
+# That content can include blog posts, video or podcast scripts, ebooks
+# or whitepapers, press releases, product category descriptions, landing 
+# page or social media copy ... and more.
+# 
+# some tips for write different content
+# 
+# 1. Write unique and original content, and go above-and-beyond what you find online.
+# 
+# 2. Write a good hook to grab your reader's attention.
+# 
+# 3. SEO-optimize your content for search engines.
+# 
+# 4. Consider how you can attract an audience across a wide variety of platforms.
+# 
+# 5. Incorporate multimedia components to break up the text.
+# 
+# 6. Segue into appropriate and relevant calls-to-action.
+# 
+# 7. Edit, edit, edit.
+
+# Question 3.What is content marketing distribution, and what are the different methods for
+# distribution?
+# 
+# Ans: Content distribution is the process of sharing, publishing, and promoting your content. 
+# It's how you provide your content to your audience members for their consumption through various
+# channels and media formats.
+# 
+# There are four types of distribution channels that exist:
+#     
+# direct selling, selling through intermediaries, dual distribution, and reverse logistics channels.
+# Each of these channels consist of institutions whose goal is to manage the transaction and physical 
+# exchange of products.
+# 
+
+# Question 4.What are the different tools for writing content?
+# 
+# Ans:25 recommended content writing tools for marketing teams
+#     
+#     Planning: 1.Answer the public
+#         2. Google Trends
+#         3. Hubspot’s Blog Topic Generator
+#         4. Portent’s Content Idea Generator
+#         5.Ideaflip
+#         6. Evernote
+#         7. Feedly
+#         8. Semrush
+#         9. Trello
+#         
+#     Drafting:
+#         10. Google Docs
+#         11. Ilys
+#         12. Coschedule Headline Analyzer
+#         13. TitleCase
+#         14. Canva
+#         15. Biteable Infografic Video Maker
+#         
+#     Editing:
+#         16. Grammarly
+#         17. Thesaurus.com
+#         18. StackEdit
+#         19. Yoast
+#         
+#     Productivity
+#     
+#      20. Stay Focused
+#      21. A Soft Murmur
+#      22. OneTab 
+#      23. Tomato Timer
+#      24. Rescue Time
+#      25. Todoist  
+
+# Question 5.What are the various graphic content creation tools on the market?
+# 
+# Ans:The various content tools 
+# 1.Adobe Express. 
+# 2.BeFunky Graphic Designer. 
+# 3.Canva. 
+# 4.Fotor. 
+# 5.Image Quote. 
+# 6.Pablo. 
+# 7.Photo Collage. 
+# 8.Pixlr.
+# 9.Quotes cover
+# 10.Snappa
+# 11.stencil
+# 12.Easel.ly
+# 13.Google charts
+# 14.infogram
+# 15.ThinkLink
+# 16.venngage
+
+# Question 6. What is a written content hemming tool?
+# 
+# Ans:The app highlights lengthy, complex sentences and common errors; if you see a yellow sentence, shorten or split it. If you see a red highlight, your sentence is so dense and complicated that your readers will get lost trying to follow its meandering, splitting logic — try editing this sentence to remove the red.
+# 
+# You can utilize a shorter word in place of a purple one. Mouse over them for hints.
+# 
+# Adverbs and weakening phrases are helpfully shown in blue. Get rid of them and pick words with force, perhaps.
+# 
+# Phrases in green have been marked to show passive voice.
+# 
+# You can format your text with the toolbar.
+# 
+# Paste in something you're working on and edit away. Or, click the Write button and compose something new.
